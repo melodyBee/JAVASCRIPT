f3fb509e4b62d995a8321936ddd44692e96b454e
melodyBee <abeerafatimarub@gmail.com>
2023-05-15 03:08:12 +0500

    completed assignemnt 1

--- Assignments/Assignment-1/app.js
+++ Assignments/Assignment-1/app.js
@@ -1,1 +1,21 @@
-alert("hello")
+//Alert
+
+//1. Write a script to greet your website visitor using JS alert box.
+alert("Hello! Welcome.");
+
+//2. Write a script to display following message on your web page:
+alert("Error! Please enter a valid password.");
+
+//3. Write a script to display following message on your web page: (Hint : Use line break)
+alert("Welcome to JS Land...\nHappy Coding!");
+
+//4. Write a script to display following messages in sequence:
+alert("Welcome to JS Land...");
+alert("Happy Coding!");
+//5. Generate the following message through browser’s developer console:
+//done
+//6. Make use of alerts in your new/existing HTML & CSS project.
+var zakat_percentage= 0.025;
+var user_input=prompt("Enter your number","00");
+var result= +user_input*zakat_percentage;
+alert("your zakat value is "+ result);
