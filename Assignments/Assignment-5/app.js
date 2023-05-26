7779323301de65c38ba0a2212496f28ad85b646f
melodyBee <abeerafatimarub@gmail.com>
2023-05-27 04:19:23 +0500

    Assignment 5 task 8

--- Assignments/Assignment-5/app.js
+++ Assignments/Assignment-5/app.js
@@ -1,6 +1,7 @@
 // MATH EXPRESSIONS
 // 1. Write a program that take two numbers & add them in a 
 // new variable. Show the result in your browser.
+document.write('<h3> Task 1 </h3>');
 var a=prompt("enter a number");
 var b=prompt("enter another number");
 var num1= parseInt(a);
@@ -10,6 +11,8 @@ document.write('sum of the following two number is '+sum+'<br>');
 
 // 2. Repeat task1 for subtraction, multiplication, division &
 // modulus.
+document.write('<h3> Task 2 </h3>');
+
 var sub=num1-num2;
 document.write('sub of the following two number is ' + sub +'<br>');
 document.write('multi of the following two number is '+num1*num2+'<br>');
@@ -17,6 +20,8 @@ document.write('div of the following two number is '+num1/num2+'<br>');
 document.write('remainder of the following two number is '+num1%num2+'<br>');
 
 // 3. Do the following using JS Mathematic Expressions
+document.write('<h3> Task 3 </h3>');
+
 // a. Declare a variable.
 var variablee;
 
@@ -42,15 +47,19 @@ document.write(--variablee+'<br>');
 // k. Show the remainder after dividing the variable’s value by 3. 
 // l. Output : “The remainder is : 0”.
 remainder= variablee%3; document.write("Remainder after dividing by 3="+remainder+'<br>')
+
 // 4. Cost of one movie ticket is 600 PKR. Write a script to store
 // ticket price in a variable & calculate the cost of buying 5 tickets to a movie. Example output:
+document.write('<h3> Task 4 </h3>');
 var ticket_price=600;
 var five_ticketPrice=5*600;
-document.write('Cost of buying 5 tickets'+five_ticketPrice+'Rs'+'<br>');
+document.write('Cost of buying 5 tickets is ='+five_ticketPrice+'Rs'+'<br>');
+
 // 5. Write a script to display multiplication table of any number in your browser.
+document.write('<h3> Task 5 </h3>');
 for(i=1;i<11;i++){
     multi=i*6;
-    console.log(6 +'x'+ i+'='+multi);
+    document.write(6 +'x'+ i+'='+multi+'<br>');
 }
 // 6. The Temperature Converter: It’s hot out! Let’s make a converter based on the steps here.
 // a. Store a Celsius temperature into a variable.
@@ -58,12 +67,13 @@ for(i=1;i<11;i++){
 // c. Now store a Fahrenheit temperature into a variable.
 // d. Convert it to Celsius & output “NNoF is NNoC”.
 // Conversion Formulae:
-
+document.write('<h3>Task 6</h3>');
 var celcius= 50;
 var fahrenheit= (celcius *9/5)+32;
-document.write('50 Celcius is '+fahrenheit+'fahrenheit'+'<br>');
+document.write('50 C is='+fahrenheit+'F'+'<br>');
 
-
+var C=(fahrenheit-32)*5/9;
+document.write('C of '+fahrenheit+'F is='+C+'<br>');
 
 // 7. Write a program to implement checkout process of a 
 // shopping cart system for an e-commerce website. Store 
@@ -74,26 +84,40 @@ document.write('50 Celcius is '+fahrenheit+'fahrenheit'+'<br>');
 // d. Ordered Quantity of item 2
 // e. Shipping charges
 // Compute the total cost & show the receipt in your browser.
+document.write('<h3>Task 7</h3>');
+var item_1=650;
+var quantity_1=3;
+var item_2=100;
+var quantity_2=7;
+var shipping_charges=100;
+var total_amount= (item_1*quantity_1)+(item_2*quantity_2)+shipping_charges;
+document.write('Price of item 1='+ item_1  +'<br>'+'Quantity of item 1 is='+ quantity_1 +'<br>'+'Price of item 2 is='+ item_2  +'<br>'+'Quantity of item 2 is='+ quantity_2 + '<br>' +'Shipping Charges='+ shipping_charges + '<br>' + 'Total amount of your order is='+ total_amount );
 // 8. Store total marks & marks obtained by a student in 2 
-// variables. Compute the percentage & show the result in 
-// your browser
-// MATH EXPRESSIONS | JAVASCRIPT
-// Page 6 of 9
+// variables. Compute the percentage & show the result in your browser
+document.write('<h3> Task 8 </h3>');
+var total_marks=980;
+var marks_obt= 804;
+var percentage= (marks_obt/total_marks)*100; 
+document.write('Total marks= '+ total_marks + '<br>'+ 'Marks obtained='+ marks_obt + '<br>' + 'Percentage= '+ percentage + '%');
 // 9. Assume we have 10 US dollars & 25 Saudi Riyals. Write a 
 // script to convert the total currency to Pakistani Rupees. 
 // Perform all calculations in a single expression.
 // (Exchange rates : 1 US Dollar = 104.80 Pakistani Rupee 
 // and 1 Saudi Riyal = 28 Pakistani Rupee)
-// MATH EXPRESSIONS | JAVASCRIPT
-// Page 7 of 9
+
+document.write('<h3>  </h3>');
+
 // 10. Write a program to initialize a variable with some 
 // number and do arithmetic in following sequence:
+document.write('<h3>  </h3>');
 // a. Add 5
 // b. Multiply by 10
 // c. Divide the result by 2
 // Perform all calculations in a single expression
+
 // 11. The Age Calculator: Forgot how old someone is? 
 // Calculate it!
+document.write('<h3>  </h3>');
 // a. Store the current year in a variable.
 // b. Store their birth year in a variable.
 // c. Calculate their 2 possible ages based on the stored 
@@ -101,9 +125,8 @@ document.write('50 Celcius is '+fahrenheit+'fahrenheit'+'<br>');
 // Output them to the screen like so: “They are either NN or NN
 // years old”.
 // 12. The Geometrizer: Calculate properties of a circle.
+document.write('<h3>  </h3>');
 // a. Store a radius into a variable.
-// MATH EXPRESSIONS | JAVASCRIPT
-// Page 8 of 9
 // b. Calculate the circumference based on the radius, and
 // output “The circumference is NN”.
 // (Hint : Circumference of a circle = 2 π r , π = 3.142)
@@ -112,6 +135,8 @@ document.write('50 Celcius is '+fahrenheit+'fahrenheit'+'<br>');
 // 13. The Lifetime Supply Calculator: Ever wonder how
 // much a “lifetime supply” of your favorite snack is? 
 // Wonder no more.
+document.write('<h3>  </h3>');
+
 // a. Store your favorite snack into a variable
 // b. Store your current age into a variable.
 // c. Store a maximum age into a variable.
