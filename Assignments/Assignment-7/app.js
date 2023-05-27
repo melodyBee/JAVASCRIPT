49508305289be71f477a0a5aea8ec7e3cc35b584
melodyBee <abeerafatimarub@gmail.com>
2023-05-28 00:47:37 +0500

    Assignment 7 Task 6

--- Assignments/Assignment-7/app.js
+++ Assignments/Assignment-7/app.js
@@ -2,6 +2,7 @@
 // 1. Write a program to take “city” name as input from user. If 
 // user enters “Karachi”, welcome the user like this: 
 // “Welcome to city of lights”
+document.write('<h3> Task 1 </h3>');
  var city= prompt("Enter your city");
  if(city=='Karachi'){
     alert("Welcome to the city of lights");
@@ -10,6 +11,7 @@
 // 2. Write a program to take “gender” as input from user. If the
 // user is male, give the message: Good Morning Sir. If the 
 // user is female, give the message: Good Morning Ma’am.
+document.write('<h3> Task 2 </h3>');
 var gender= prompt("Enter your gender");
 if(gender=='male'){
 alert("Good morning Sir");
@@ -24,6 +26,7 @@ if(gender=='female'){
 // Red Must Stop
 // Yellow Ready to move
 // Green Move now
+document.write('<h3> Task 3 </h3>');
 var signalColor=prompt('Enter the traffic light color\nRead\nYellow\nGreen');
 switch(signalColor){
     case "Red": alert("Must stop!"); break;
@@ -34,10 +37,13 @@ switch(signalColor){
 // 4. Write a program to take input remaining fuel in car (in 
 // litres) from user. If the current fuel is less than 0.25litres, 
 // show the message “Please refill the fuel in your car”
+document.write('<h3> Task 4 </h3>');
 
 // 5. Run this script, & check whether alert message would be
 // displayed or not. Record the outputs.
 // a. 
+document.write('<h3> Task 5 </h3>');
+
 var a = 4;
 if (++a === 5){
 alert("given condition for variable a is true");
@@ -81,15 +87,47 @@ alert("False");
 alert("car is smaller than cat");
 }
 
-
 // 6. Write a program to take input the marks obtained in three
 // subjects & total marks. Compute & show the resulting
 // percentage on your page. Take percentage & compute 
 // grade as per following table:
+document.write('<h3> Task 6 </h3>');
+var sub1Marks= prompt("Enter subject 1 Marks");
+var sub2Marks=prompt("Enter subject 2 Marks");
+var sub3Marks=prompt("Enter subject 3 Marks");
+var sub1Marks= +sub1Marks;
+var sub2Marks= +sub2Marks;
+var sub3Marks= +sub3Marks;
+var marks_obt= sub1Marks + sub2Marks + sub3Marks;
+var per=(marks_obt/totalMarks)*100;
+document.write("Total marks : "+ 300);
+document.write('<br>Marks obtained : '+ marks_obt );
+document.write('<br><br>Percentage : '+ per);
+if(80<per){
+    document.write('<br>Grade:  A-one');
+    document.write('<br>Reamrks : Excellent');
+}
+else
+if(70<per){
+    document.write('<br>Grade: A');
+    document.write('<br>Reamrks : Good');
+}
+else
+if(60<per){
+    document.write('<br>Grade:  B');
+    document.write('<br>Reamrks : You need to improve');
+}
+else
+if(60>per){
+    document.write('<br>Grade:  Fail');
+    document.write('<br>Reamrks : Sorry');
+}
 // Show the total marks, marks obtained, percentage, grade &
 // remarks like:
 
 // 7. Guess game:
+document.write('<h3> Task 7 </h3>');
+
 // Store a secret number (ranging from 1 to 10) in a variable.
 // Prompt user to guess the secret number.
 // a. If user guesses the same number, show “Bingo! Correct
@@ -101,12 +139,16 @@ alert("car is smaller than cat");
 // 8. Write a program to check whether the given number is
 // divisible by 3. Show the message to the user if the number 
 // is divisible by 3.
+document.write('<h3> Task 8 </h3>');
 
 // 9. Write a program that checks whether the given input is an
 // even number or an odd number.
+document.write('<h3> Task 9 </h3>');
 
 // 10. Write a program that takes temperature as input and 
 // shows a message based on following criteria
+document.write('<h3> Task10 </h3>');
+
 // a. T > 40 then “It is too hot outside.”
 // b. T > 30 then “The Weather today is Normal.”
 // c. T > 20 then “Today’s Weather is cool.”
@@ -114,6 +156,8 @@ alert("car is smaller than cat");
 
 // 11. Write a program to create a calculator for +,-,*, / & % 
 // using if statements. Take the following input:
+document.write('<h3> Task 11 </h3>');
+
 // a. First number
 // b. Second number
 // c. Operation (+, -, *, /, %)
