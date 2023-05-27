a54a92e3041798e07d024ad78a8e14fc25f7525a
melodyBee <abeerafatimarub@gmail.com>
2023-05-27 16:47:18 +0500

    Assignment 6 Task 4

--- Assignments/Assignment-6/app.js
+++ Assignments/Assignment-6/app.js
@@ -2,6 +2,7 @@
 // 1. Write a program to take a number in a variable, do the
 // required arithmetic to display the following result in your
 // browser:
+document.write('<h3> Task 1 </h3>');
 var a=10;
 document.write('Result: <br>The value of a is: '+ a);
 document.write('<br>----------------------<br>');
@@ -26,23 +27,31 @@ document.write('<br>Now the value of a is: '+ a);
 // --a - --b;
 // --a - --b + ++b;
 // --a - --b + ++b + b--;
+document.write('<h3> Task 2 </h3>');
 var a=2, b=1;
-document.write('a is: '+ a);
-document.write('b is: '+ b);
+document.write('<br>a is: '+ a);
+document.write('<br>b is: '+ b);
 var result = --a - --b + ++b + b--;
-document.write('result is: '+ result);
+document.write('<br>result is: '+ result +'<br><br>');
 
 // 3. Write a program that takes input a name from user & 
 // greet the user.
-var input= prompt("Enter your name");
-alert("Welcome "+ input);
+document.write('<h3> Task 3 </h3>');
+var inputName= prompt("Enter your name");
+alert("Welcome "+ inputName);
 
 // 4.
 // 5. Write a program to take input a number from user & 
 // display it’s multiplication table on your browser. If user 
 // does not enter a new number, multiplication table of 5 
 // should be displayed by default.
-
+document.write('<h3> Task 4 </h3>');
+var inputNum= prompt("Enter a number");
+var inputNum= +inputNum;
+for(i=1;i<11;i++){
+    multi=i*inputNum;
+    document.write(inputNum +'x'+ i+'='+multi+'<br>');
+}
 // 6. Take
 // a) Take three subjects name from user and store them in 3 
 // different variables.
@@ -50,9 +59,8 @@ alert("Welcome "+ input);
 // variable.
 // c) Take obtained marks for first subject from user and 
 // stored it in different variable.
-// ALERTS | JAVASCRIPT
-// Page 3 of 3
 // d) Take obtained marks for remaining 2 subjects from user 
 // and store them in variables.
 // e) Now calculate total marks and percentage and show the 
 // result in browser like this.(Hint: user table)
+document.write('<h3> Task 5 </h3>');
