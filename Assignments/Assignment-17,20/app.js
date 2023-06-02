64bdb2af9692396b1b9a6b81543cab29708a9704
melodyBee <abeerafatimarub@gmail.com>
2023-06-02 06:53:00 +0500

    idk I just did something

--- Assignments/Assignment-17,20/app.js
+++ Assignments/Assignment-17,20/app.js
@@ -34,7 +34,7 @@ document.write('<h3> Task 5 </h3>');
 // fruits = [“apple”, “banana”, “mango”, “orange”, 
 // “strawberry”]
 var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
-for (var i = 0; i < fruits.length; i++) {
+for (i = 0; i < fruits.length; i++) {
   console.log(fruits[i]);
 }
 // 6. Generate the following series in your browser. See 
@@ -44,35 +44,35 @@ document.write('<h3> Task 6 </h3>');
 // a. Counting: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
 
 document.write("Counting: ");
-for (var i = 1; i <= 15; i++) {
+for (i=1;i<15;i++) {
   document.write(i + ", ");
 }
 document.write("<br>");
 
 // b. Reverse counting: 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
 document.write("Reverse counting: ");
-for (var i = 10; i >= 1; i--) {
+for (i = 10; i >= 1; i--) {
   document.write(i + ", ");
 }
 document.write("<br>");
 
 // c. Even: 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20
 document.write("Even: ");
-for (var i = 0; i <= 20; i += 2) {
+for (i = 0; i <= 20; i += 2) {
   document.write(i + ", ");
 }
 document.write("<br>");
 
 // d. Odd: 1, 3, 5, 7, 9, 11, 13, 15, 17, 19
 document.write("Odd: ");
-for (var i = 1; i < 20; i += 2) {
+for (i = 1; i < 20; i += 2) {
   document.write(i + ", ");
 }
 document.write("<br>");
 
 // e. Series: 2k, 4k, 6k, 8k, 10k, 12k, 14k, 16k, 18k, 20k
 document.write("Series: ");
-for (var i = 2; i <= 20; i += 2) {
+for (i = 2; i <= 20; i += 2) {
   document.write(i + "k, ");
 }
 
@@ -110,7 +110,7 @@ console.log(smallest);
 
 // 10. Write a program to print multiples of 5 ranging 1 to 100.
 document.write('<h3> Task 10 </h3>');
-for (var i = 1; i <= 100; i++) {
+for (i = 1; i <= 100; i++) {
   if (i % 5 == 0) {
     console.log(i);
   }
--- Assignments/Assignment-53-57/app.js
+++ Assignments/Assignment-53-57/app.js
@@ -2,22 +2,43 @@
 // 1. Consider you have 4 images in a file as shown below: 
 // Now When you click on an image it should display in a modal.
 // Modal code is available in this assignment file.
-function openModal(imageSrc) {
-  var modal = document.getElementById("modal");
-  var modalImage = document.getElementById("modal-image");
-  modal.style.display = "block";
-  modalImage.src = imageSrc;
-}
 
-function closeModal() {
-  var modal = document.getElementById("modal");
-  modal.style.display = "none";
+function openModal(imageSrc) {
+  var modal = document.getElementById("myModal");
+  var modalImg = document.getElementById("modalImage");
+  var closeBtn = document.getElementsByClassName("close")[0];
+
+  modal.style.display = "block";
+  modalImg.src = imageSrc;
+
+  // Close modal when the user clicks on the close button (X)
+  closeBtn.onclick = function() {
+      closeModal();
+  }
+
+  // Close modal when the user clicks anywhere outside the image
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          closeModal();
+      }
+  }
+
+  function closeModal() {
+      modal.style.display = "none";
+  }
 }
 
 // 2. Create a paragraph and two buttons “zoom in ”(+) and “zoom out”(-).
 // On each click on “zoom in”(+) , add 10px in font size of paragraph.
 // And on each click on “zoom out”(-) , minus 10px in font size of paragraph.
 
+function zoomIn(img) {
+  document.getElementById("zoom-image").style.display = "block";
+  document.getElementById("zoom-image").getElementsByTagName("img")[0].src = img.src;
+}
+function zoomOut() {
+  document.getElementById("zoom-image").style.display = "none";
+}
 
 function zoomIn() {
   var para = document.getElementById("text");
